3226baabbc32e919145d38a2cde093f089d33e2a
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-05-19 11:39:07 +0200

    BBDD

---
+++ BaseDaros.sql
@@ -0,0 +1,205 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: programacion
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `Usuario` varchar(100) NOT NULL,
+  `Contraseña` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`Usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES ('',''),('Josema','Josema'),('Josema2','Josema2'),('Paco','XD');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `Id_Empleado` int NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Sueldo` double DEFAULT NULL,
+  `Puesto` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Id_Empleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,'José Manuel Luque',1250,'Taquillero');
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entradas`
+--
+
+DROP TABLE IF EXISTS `entradas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entradas` (
+  `Codigo` decimal(2,0) NOT NULL,
+  `Usuario` varchar(9) DEFAULT NULL,
+  PRIMARY KEY (`Codigo`),
+  KEY `Usuario` (`Usuario`),
+  CONSTRAINT `entradas_ibfk_1` FOREIGN KEY (`Usuario`) REFERENCES `usuarios` (`Usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entradas`
+--
+
+LOCK TABLES `entradas` WRITE;
+/*!40000 ALTER TABLE `entradas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entradas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `peliculas`
+--
+
+DROP TABLE IF EXISTS `peliculas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `peliculas` (
+  `Titulo` varchar(100) NOT NULL,
+  `Duracion` decimal(3,0) DEFAULT NULL,
+  `Genero` varchar(50) DEFAULT NULL,
+  `Año` decimal(4,0) DEFAULT NULL,
+  `Portada` varchar(100) DEFAULT NULL,
+  `PosicionCartelera` int DEFAULT NULL,
+  PRIMARY KEY (`Titulo`),
+  UNIQUE KEY `Portada` (`Portada`),
+  UNIQUE KEY `PosicionCartelera_UNIQUE` (`PosicionCartelera`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peliculas`
+--
+
+LOCK TABLES `peliculas` WRITE;
+/*!40000 ALTER TABLE `peliculas` DISABLE KEYS */;
+INSERT INTO `peliculas` VALUES ('Avatar: El sentido del agua',192,'Ciencia Ficción',2022,'Películas\\Avatar.jpg',1),('Elemental',102,'Animacion',2023,'Películas\\elemental.jpg',6),('Flash',140,'Super Héroes',2023,'Películas\\Flash.jpg',5),('Guardianes de la Galaxia: Vol. 3',149,'Ciencia Ficción',2023,'Películas\\GG.jpg',3),('Spider-Man: Cruzando el multiverso',136,'Animacion',2023,'Películas\\SM.jpg',4),('Super Mario Bros: La película',92,'Animación',2023,'Películas\\SMB.jpg',2);
+/*!40000 ALTER TABLE `peliculas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salas`
+--
+
+DROP TABLE IF EXISTS `salas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salas` (
+  `Numero` decimal(2,0) NOT NULL,
+  `Filas` decimal(2,0) DEFAULT NULL,
+  `Columnas` decimal(2,0) DEFAULT NULL,
+  `numButacasTotal` decimal(3,0) DEFAULT NULL,
+  PRIMARY KEY (`Numero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salas`
+--
+
+LOCK TABLES `salas` WRITE;
+/*!40000 ALTER TABLE `salas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `salas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sesion`
+--
+
+DROP TABLE IF EXISTS `sesion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sesion` (
+  `Sala` decimal(2,0) DEFAULT NULL,
+  `Hora` varchar(9) DEFAULT NULL,
+  KEY `Sala` (`Sala`),
+  CONSTRAINT `sesion_ibfk_1` FOREIGN KEY (`Sala`) REFERENCES `salas` (`Numero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sesion`
+--
+
+LOCK TABLES `sesion` WRITE;
+/*!40000 ALTER TABLE `sesion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sesion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `Usuario` varchar(100) NOT NULL,
+  `Contraseña` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`Usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES ('','wddawd@gmail.com'),('a','a'),('abcd./2','abcd./2'),('paco','PQUI'),('user','password');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-19 11:38:41
